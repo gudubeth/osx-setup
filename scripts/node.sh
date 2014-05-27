e3c497a0c69106c84d9a3a9e1cad5577d1e91d51
Ozgur Erdem <gudubeth@gmail.com>
2014-05-27 15:51:06 +0300

    added nodejs

--- scripts/node.sh
+++ scripts/node.sh
@@ -3,3 +3,10 @@
 # Assumes: 
 # + Homebrew has been installed correctly.
 # + Homebrew-cask has been tapped and installed correctly.
+
+#npm will be installed with node
+brew install node
+
+npm install -g grunt-cli        #js task runner
+
+npm install -g bower            #package manager for the web
